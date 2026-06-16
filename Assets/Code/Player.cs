bc8b8a12e467b7b4b25b4f90dac5ac0060e6dced
123artCode <art-faden0o@icloud.com>
2026-06-16 15:41:55 +0200

    added input system

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,17 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ExampleClass : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    void setup()
+    private InputSystem_Actions.InputSystem_Actions inputActions;
+    private Vector2 moveInput;
+    [HideInInspector]
+    public float speed = 5f;        
+    [SerializeField]
+    public Rigidbody rb;
+    
+
+    private void Awake()
     {
-        
+        inputActions = new InputSystem_Actions.InputSystem_Actions();
+        Debug.Log(rb);
     }
-    //movement speed in units per second
-    private float movementSpeed = 5f;
 
-    void Update()
+    private void OnEnable()
     {
-        transform.position = transform.position + new Vector3(1, 0, 3);
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+        inputActions.Player.Jump.performed += OnJump;
+    }
 
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Jump.performed -= OnJump;
+        inputActions.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
     }
 }
